70012959eb17c5ee3ac41421a2066def057ffbad
NateRobinson <840501291@qq.com>
2018-06-15 19:02:37 +0800

    commit

---
+++ natelibraryone/src/main/java/com/nate/libraryone/TestApiOne.java
@@ -0,0 +1,9 @@
+package com.nate.libraryone;
+
+public class TestApiOne {
+
+    public static String getName(){
+        return "TestApiOne";
+    }
+
+}
---
+++ notelibrarytwo/src/main/java/com/note/librarytwo/TestApiTwo.java
@@ -0,0 +1,9 @@
+package com.note.librarytwo;
+
+public class TestApiTwo {
+
+    public static String getName(){
+        return "TestApiTwo";
+    }
+
+}
